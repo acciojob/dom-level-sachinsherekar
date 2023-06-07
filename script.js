154dde79a95394ce23f58d523145a8d2b749ddc6
sachinsherekar <130824306+sachinsherekar@users.noreply.github.com>
2023-06-07 15:55:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+//var a =document.getElementById("level").value;
+//alert("The level of the element is:"+a);
+var a = document.getElementById("level").value;
+
+alert("The level of the element is: " + a);//
